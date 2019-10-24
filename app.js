083ade24d935946c092494218666068962878625
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2019-10-24 11:46:51 -0400

    10-24-19(Th)
    We are being drafted out today. We are going to be at war....

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const DOMStrings = {
   input: document.getElementById("pkmn-form"),
   name: document.getElementById("pokemon-name"),
-  displayName: document.querySelector('.pkmon-name-size'),
-  displayNumber: document.querySelector('.pkmn-num'),
-  displayImageFront: document.querySelector('.display-image-front-def'),
-  displayImageBack: document.querySelector('.display-image-back-def'),
-  displayImageShinyFront:document.querySelector('.display-image-shiny-front'),
-  displayImageShinyBack: document.querySelector('.display-image-shiny-back')
+  displayName: document.querySelector(".pkmn-name-size"),
+  displayImageFront: document.querySelector(".display-image-front-def"),
+  displayImageBack: document.querySelector(".display-image-back-def"),
+  displayImageShinyFront: document.querySelector(".display-image-shiny-front"),
+  displayImageShinyBack: document.querySelector(".display-image-shiny-back"),
+  displayNum: document.querySelector(".pkmn-num"),
+  type: document.querySelector(".type")
 };
+
+DOMStrings.input.addEventListener("keypress", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".nes-btn").click();
+  }
+});
 
 function getPkmn() {
   DOMStrings.input.addEventListener("submit", async function(e) {
@@ -20,7 +28,17 @@ function getPkmn() {
       const data = await result.json();
       //console.log(data);
 
-      const displayPkmn = function(data) {};
+      const displayPkmn = function(data) {
+        DOMStrings.displayName.innerText = data.name;
+        DOMStrings.displayNum.innerText = data.id;
+        DOMStrings.displayImageFront.src = data.sprites.front_default;
+        DOMStrings.displayImageBack.src = data.sprites.back_default;
+        DOMStrings.displayImageShinyFront.src = data.sprites.front_shiny;
+        DOMStrings.displayImageShinyBack.src = data.sprites.back_shiny;
+        DOMStrings.type.textContent = data.types.map(data => data.type.name);
+      };
+
+      displayPkmn(data);
     } catch (err) {
       console.log(err);
     }
